a2c2c95e4f474beabac6d3d0f7204678ecccb5cb
hmm007 <hmmpython@gmail.com>
2015-02-27 12:10:52 +0100

    Update VisMotif.py

--- VisMotif/VisMotif.py
+++ VisMotif/VisMotif.py
@@ -1,9 +1,9 @@
-from random import Random
 import os
 import re
 import sys
 import numpy
 from operator import itemgetter
+from random import Random
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
