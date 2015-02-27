1f7ba5d208edb3c4ffe0472b485b4b6da274a4da
hmm007 <hmmpython@gmail.com>
2015-02-27 12:06:58 +0100

    Create VisMotif.py

---
+++ VisMotif/VisMotif.py
@@ -0,0 +1,10 @@
+from random import Random
+import os
+import re
+import sys
+import numpy
+from operator import itemgetter
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from .query_integral_image import query_integral_image
