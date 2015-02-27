1baf400eb8b32ba8ce0fa16bf8165e3cfe8b52f8
hmm007 <hmmpython@gmail.com>
2015-02-27 12:15:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    name='VisMotif',
+    version='0.9',
+    url='https://github.com/hmm007/VisMotif',
+    license='See License File',
+    ext_modules=cythonize('VisMotif/query_integral_image.pyx'),
+    packages=['VisMotif'],
+    package_data={'VisMotif'}
+)
