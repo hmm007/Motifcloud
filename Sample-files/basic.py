62e836403ff7d53b36b74bab71d8bf10e1071cd0
hmm007 <hmmpython@gmail.com>
2016-02-13 10:31:12 +0100

    Create basic.py

---
+++ Sample-files/basic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+"""
+motif generating from sample file
+"""
+
+from os import path
+import matplotlib.pyplot as plt
+from wordcloud import VisMotif
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+tex = open(path.join(d, 'constitution.txt')).read()
+
+
+motifcloud = VisMotif().generate(tex)
+
+#print (wordcloud)
+
+# Open a plot of the generated image.
+plt.imshow(motifcloud)
+plt.axis("off")
+plt.show()
